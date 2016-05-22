af84ba0405e9e7f7841ccf99aaed8ac882b2b1bc
KuanZe <kostasladavicius@hotmail.com>
2016-05-22 21:34:35 +0300

    budgetcontrol

---
+++ Fiinasta/Fiinasta/Controllers/BudgetController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Fiinasta.FiinastaAPI;
+
+namespace Fiinasta.Controllers
+{
+    public class BudgetController : Controller
+    {
+        // GET: Budget
+        public ActionResult Run()
+        {
+            using (FiinastaAPIClient client = new FiinastaAPIClient())
+            {
+                return View("Budget", client.GetSpendings("",Session["user"] as Users).ToList());
+            }
+            
+        }
+        [HttpPost]
+        public void Budget(string criteria)
+        {
+            Url.Action("Run", "Search", new
+            {
+                @criteria = criteria
+            });
+        }
+    }
+}
