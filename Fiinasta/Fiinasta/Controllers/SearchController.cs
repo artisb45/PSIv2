69c40ea950345a6a75451c9d4f6a0e1ef0a5237e
KuanZe <kostasladavicius@hotmail.com>
2016-05-22 21:33:57 +0300

    searchcontrol

---
+++ Fiinasta/Fiinasta/Controllers/SearchController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Fiinasta.FiinastaAPI;
+
+namespace Fiinasta.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Run(string criteria)
+        {
+            using (FiinastaAPIClient client = new FiinastaAPIClient())
+            {
+                var a = client.Search(criteria, Session["user"] as Users);
+                return View("Budget", client.Search(criteria, Session["user"] as Users));
+            }
+        }
+    }
+}
